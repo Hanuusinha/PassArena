f17c63b708235a9b53eb37bfcc39815a65845286
Maruti <contact.yashsinha@gmail.com>
2026-08-30 23:42:55 +0530

    Resolved Issues in Phase-1 Development

--- config-server/src/main/java/com/passArena/config_server/ConfigServerApplication.java
+++ config-server/src/main/java/com/passArena/config_server/ConfigServerApplication.java
@@ -4,11 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+import java.net.InetAddress;
+
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServerApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
+        System.out.println("Hostname " + InetAddress.getLocalHost().getHostName());
+        System.out.println("Host Address " + InetAddress.getLocalHost().getHostAddress());
 		SpringApplication.run(ConfigServerApplication.class, args);
 	}
 
